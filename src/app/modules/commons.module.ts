5075b01c90aecaef7d8cd4155aad4e482fef1bdb
miguel <arellano.ecotecnologias@gmail.com>
2022-01-04 19:58:24 -0600

    se termino de crear el home con todo y responsive

--- src/app/modules/commons.module.ts
+++ src/app/modules/commons.module.ts
@@ -1,15 +1,22 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { HttpClientModule } from '@angular/common/http';
+import { FooterComponent } from '../components/footer/footer.component';
 
 
 @NgModule({
-    declarations: [],
+    declarations: [
+        FooterComponent,
+    ],
+
     imports: [ 
         CommonModule,
         HttpClientModule
      ],
-    exports: [],
+     
+    exports: [
+        FooterComponent,
+    ],
     providers: [],
 })
 export class CommonsModule {}
